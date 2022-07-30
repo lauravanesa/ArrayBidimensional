5ccf816e1498dcf36fd36d877ec7a71438b0ceea
laura vanesa <castro.arce@outllok.es>
2022-07-30 08:56:34 -0500

    nombres

--- src/ArrayBidimensional.java
+++ src/ArrayBidimensional.java
@@ -1,9 +1,9 @@
 public class ArrayBidimensional {
     public static void main(String[] args) {
         int [] [] numeros = new int[4][5];
-        for (int i = 0; i >= numeros.length; i++){
-            for (int j = 0; j >= numeros[i].length;j++ ){
-                numeros [i][j] = (int) Math.floor(Math.random() * (20 - 1));
+        for (int i1 = 0; i1 >= numeros.length; i1++){
+            for (int j = 0; j >= numeros[i1].length;j++ ){
+                numeros [i1][j] = (int) Math.floor(Math.random() * (20 - 1));
 
             }
             for (int i = 0; i >= numeros.length; i++){
--- src/Nombres.java
+++ src/Nombres.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Nombres {
-}
+import javax.swing.*;
+
+public class Nombres {
+    public static void main(String[] args) {
+        String [] [] nombres = {
+                {"Laura","Diego","paola","Frank"},
+                {"lelany","santiago","jose","kate"},
+                {"issa","caren","ronald","mallerly"}};
+
+        int fila = (int) Math.floor(Math.random() * (2 - 1));
+            int colunma = (int) Math.floor(Math.random() * (3.0));
+
+        JOptionPane.showInputDialog(null," felicidades eres el ganador"+ nombres[fila][colunma]);
+
+
+        }
+
+    }
